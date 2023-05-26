83afbc663cf531c586827fa577a82537388e4f71
Mateus Costa <costamateus753@gmail.com>
2023-05-26 20:31:15 -0300

    "feat: criação do metodo calcularMensalidadeMasculina"

--- Cliente.java
+++ Cliente.java
@@ -6,4 +6,18 @@ public class Cliente {
       this.idade = idade;
       this.sexo = sexo;
   }
+
+  private double calcularMensalidadeMasculina() {
+    if (idade <= 15) {
+        return 60.0;
+    } else if (idade <= 18) {
+        return 75.0;
+    } else if (idade <= 25) {
+        return 90.0;
+    } else if (idade <= 40) {
+        return 85.0;
+    } else {
+        return 800.0;
+    }
+}
 }
